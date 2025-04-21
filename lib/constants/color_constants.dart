a723c60ef4e5063c1aa17caf92c2ab39f4ac4154
Dang Nguyen <nguyen05082004@gmail.com>
2025-04-21 22:57:05 +0700

    Add folder

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorConstants{
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const blackColor = Color(0xFF000000);
+  static const logoColor = Color(0xFF146AE0);
+  static const buttonColor = Color(0xFF6783E7);
+  static const registerTextColor = Color(0xFF047DE7);
+  static const errorTextColor = Color(0xFFEC4622);
+}
