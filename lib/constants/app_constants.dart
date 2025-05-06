7928441083e365203a2f256b355e66f30e00125f
Dang Nguyen <nguyen05082004@gmail.com>
2025-05-06 09:28:27 +0700

    delete old version

--- lib/constants/app_constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants{
-  static const appTitle = 'Bkav App';
-  static const registerTitle = 'Tạo tài khoản';
-  // static const loginTitle = '';
-  static const registerButton = 'Đăng ký';
-  static const registerSubmitButton = 'Tạo tài khoản';
-  static const loginButton = 'Đăng nhập';
-}
